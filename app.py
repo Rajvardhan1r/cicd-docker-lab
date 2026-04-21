587f34691436ac323ea84bfeb9fccfc33f6fc1a9
rajvardhan <13rajrathore@gmail.com>
2026-04-21 17:14:59 +0530

    Updated application

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Rajvardhan CI/CD Docker Pipeline"
+   
+    return "Updated CI/CD Pipeline by Rajvardhan"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
